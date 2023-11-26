e3351dc774e69098cb774ecf5e68f67f2423fe3d
Baliram <heyome9a@gmail.com>
2023-11-26 19:58:23 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import './App.sass'
-import Footer from './components/footer/footer';
-import Nav from './components/nav/nav';
-import Home from "./pages/home";
-import { Routes, Route } from "react-router-dom"
-import Tp from './components/term&privacy/term&Privacy';
+import { Routes, Route } from "react-router-dom";
 
+import Tp from "./components/term&privacy/term&Privacy";
+import Footer from "./components/footer/footer";
+import Nav from "./components/nav/nav";
+// import Home from "./pages/home";
+
+import "./App.sass";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Routes>
-        <Route path="/sizeReducer" element={<Home />} />
-        <Route path="/sizeReducer/terms-privacy/" element={<Tp />} />
+        {/* <Route path="/sizeReducer" element={<Home />} />
+        <Route path="/sizeReducer/terms-privacy/" element={<Tp />} /> */}
+        <Route path="/sizeReducer" element={<Tp />} />
       </Routes>
       <Footer />
     </div>
