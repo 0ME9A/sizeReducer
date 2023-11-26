29e603a88104fe598f69abe310429244b211a923
Baliram <heyome9a@gmail.com>
2023-11-26 20:01:59 +0530

    deploy test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
 
-import Tp from "./components/term&privacy/term&Privacy";
+// import Tp from "./components/term&privacy/term&Privacy";
 import Footer from "./components/footer/footer";
 import Nav from "./components/nav/nav";
 // import Home from "./pages/home";
@@ -11,11 +11,10 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      <Routes>
-        {/* <Route path="/sizeReducer" element={<Home />} />
-        <Route path="/sizeReducer/terms-privacy/" element={<Tp />} /> */}
-        <Route path="/sizeReducer" element={<Tp />} />
-      </Routes>
+      {/* <Routes>
+        <Route path="/sizeReducer" element={<Home />} />
+        <Route path="/sizeReducer/terms-privacy/" element={<Tp />} />
+      </Routes> */}
       <Footer />
     </div>
   );
